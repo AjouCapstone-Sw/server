870b83e917f4949434d6d03be10e5c73c8ed1751
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-28 18:43:48 +0900

    feat: webRTC Skeleton code

---
+++ socket.js
@@ -0,0 +1,259 @@
+import { Server, Socket } from "socket.io";
+
+export const SocketMap = {};
+
+export const socketInit = (server, app) => {
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  app.set("io", io);
+  io.on("connection", (socket) => {
+    const req = socket.request;
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    console.log("socket 연결 성공 ip : ", ip);
+    console.log(socket.id);
+
+    //여기 부터 chating
+
+    socket.on("setUid", (Id) => {
+      SocketMap[Id] = socket.id;
+      console.log(SocketMap);
+    });
+
+    //  여기부터 rtc
+
+    socket.on("joinRoom", ({ id, chatRoomId }) => {
+      try {
+        let allUsers = getOtherUsersInRoom(id, chatRoomId);
+        io.to(id).emit("allUsers", { users: allUsers });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    socket.on("senderOffer", async (data) => {
+      try {
+        socketToRoom[data.senderSocketID] = data.roomId;
+        let pc = createReceiverPeerConnection(
+          data.senderSocketID,
+          app.get("io"),
+          data.roomId
+        );
+        await pc.setRemoteDescription(data.sdp);
+        let answerSdp = await pc.createAnswer({
+          offerToReceiveAudio: true,
+          offerToReceiveVIdeo: true,
+        });
+        await pc.setLocalDescription(answerSdp);
+        socket.join(data.roomId);
+        io.to(data.senderSocketID).emit("getSenderAnswer", { sdp: answerSdp });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    socket.on("senderCandidate", async (data) => {
+      try {
+        let pc = receiverPCs[data.senderSocketID];
+        await pc.addIceCandidate(new wrtc.RTCIceCandidate(data.candidate));
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    socket.on("receiverOffer", async (data) => {
+      try {
+        let pc = createSenderPeerConnection(
+          data.receiverSocketID,
+          data.senderSocketID,
+          app.get("io"),
+          data.roomID
+        );
+        await pc.setRemoteDescription(data.sdp);
+        let sdp = await pc.createAnswer({
+          offerToReceiveAudio: false,
+          offerToReceiveVIdeo: false,
+        });
+        await pc.setLocalDescription(sdp);
+        app.get("io").to(data.receiverSocketID).emit("getReceiverAnswer", {
+          id: data.senderSocketID,
+          sdp,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    socket.on("receiverCandidate", async (data) => {
+      try {
+        const senderPC = senderPCs[data.senderSocketID].filter(
+          (sPC) => sPC.id === data.receiverSocketID
+        )[0];
+        await senderPC.pc.addIceCandidate(
+          new wrtc.RTCIceCandidate(data.candidate)
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    });
+    socket.on("leaveRoom", () => {
+      try {
+        let roomId = socketToRoom[socket.id];
+
+        deleteUser(socket.id, roomId);
+        closeReceiverPC(socket.id);
+        closeSenderPCs(socket.id);
+        app.get("io").to(roomId).emit("userExit", socket.id);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+    socket.on("disconnect", () => {
+      try {
+        let roomId = socketToRoom[socket.id];
+
+        deleteUser(socket.id, roomId);
+        closeReceiverPC(socket.id);
+        closeSenderPCs(socket.id);
+        app.get("io").to(roomId).emit("userExit", socket.id);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  });
+};
+
+let receiverPCs = {};
+let senderPCs = {};
+let users = {};
+let socketToRoom = {};
+
+const pc_config = {
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+  ],
+};
+
+const isIncluded = (array, Id) => array.some((item) => item.id === Id);
+
+const createReceiverPeerConnection = (socketId, socket, roomId) => {
+  const pc = new wrtc.RTCPeerConnection(pc_config);
+
+  if (receiverPCs[socketId]) receiverPCs[socketId] = pc;
+  else receiverPCs = { ...receiverPCs, [socketId]: pc };
+
+  pc.onicecandidate = (e) => {
+    socket.to(socketId).emit("getSenderCandidate", {
+      candidate: e.candidate,
+    });
+  };
+
+  pc.ontrack = (e) => {
+    if (users[roomId]) {
+      if (!isIncluded(users[roomId], socketId)) {
+        users[roomId].push({
+          id: socketId,
+          stream: e.streams[0],
+        });
+      } else return;
+    } else {
+      users[roomId] = [
+        {
+          id: socketId,
+          stream: e.streams[0],
+        },
+      ];
+    }
+    socket.to(roomId).emit("userEnter", { id: socketId });
+  };
+
+  return pc;
+};
+
+const createSenderPeerConnection = (
+  receiversocketId,
+  sendersocketId,
+  socket,
+  roomId
+) => {
+  const pc = new wrtc.RTCPeerConnection(pc_config);
+
+  if (senderPCs[sendersocketId]) {
+    senderPCs[sendersocketId].filter((user) => user.id !== receiversocketId);
+    senderPCs[sendersocketId].push({ id: receiversocketId, pc });
+  } else
+    senderPCs = {
+      ...senderPCs,
+      [sendersocketId]: [{ id: receiversocketId, pc }],
+    };
+
+  pc.onicecandidate = (e) => {
+    socket.to(receiversocketId).emit("getReceiverCandidate", {
+      id: sendersocketId,
+      candidate: e.candidate,
+    });
+  };
+
+  const sendUser = users[roomId].filter(
+    (user) => user.id === sendersocketId
+  )[0];
+  sendUser.stream.getTracks().forEach((track) => {
+    pc.addTrack(track, sendUser.stream);
+  });
+
+  return pc;
+};
+
+const getOtherUsersInRoom = (socketId, roomId) => {
+  let allUsers = [];
+  if (!users[roomId]) return allUsers;
+  console.log(users[roomId]);
+  allUsers = users[roomId]
+    .filter((user) => user.id !== socketId)
+    .map((otherUser) => ({
+      id: otherUser.id,
+    }));
+
+  return allUsers;
+};
+
+const deleteUser = (socketId, roomId) => {
+  if (!users[roomId]) return;
+  users[roomId] = users[roomId].filter((user) => user.id !== socketId);
+  if (users[roomId].length === 0) {
+    delete users[roomId];
+  }
+  if (!socketToRoom[socketId]) return;
+  delete socketToRoom[socketId];
+};
+
+const closeReceiverPC = (socketId) => {
+  if (!receiverPCs[socketId]) return;
+
+  receiverPCs[socketId].close();
+  delete receiverPCs[socketId];
+};
+
+const closeSenderPCs = (socketId) => {
+  if (!senderPCs[socketId]) return;
+
+  senderPCs[socketId].forEach((senderPC) => {
+    senderPC.pc.close();
+    const eachSenderPC = senderPCs[senderPC.id].filter(
+      (sPC) => sPC.id === socketId
+    )[0];
+    if (!eachSenderPC) return;
+    eachSenderPC.pc.close();
+    senderPCs[senderPC.id] = senderPCs[senderPC.id].filter(
+      (sPC) => sPC.id !== socketId
+    );
+  });
+
+  delete senderPCs[socketId];
+};
