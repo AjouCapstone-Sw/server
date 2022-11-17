547b1f24530b8efedffbb5dff4437f1140e92652
Coreight98 <maxcha98@naver.com>
2022-11-17 23:25:30 +0900

    feat: 경매 소켓 로직

---
+++ auction/auction.js
@@ -0,0 +1,75 @@
+class AuctionHouse {
+  constructor({ seller, auction, manage }) {
+    this.users = [seller];
+    this.seller = seller;
+    this.auction = auction;
+    this.op = null;
+    this.conclusionUser = null;
+    this.manage = manage;
+  }
+  getSeller() {
+    return this.seller;
+  }
+
+  join(user) {
+    this.users.push(user);
+  }
+
+  runAuction(op) {
+    this.op = op;
+  }
+
+  conclusion(user) {
+    this.conclusionUser = user;
+  }
+}
+
+class Manage {
+  constructor({ operateTime }) {
+    this.startTime = new Date().getTime();
+    this.operateTime = operateTime;
+    this.queue = [];
+  }
+  isRunning() {
+    const time = new Date().getTime();
+    return time - this.startTime < this.operateTime;
+  }
+
+  isConclusionNow() {
+    return !!this.queue.length;
+  }
+
+  getDeterminedBuyer() {
+    return this.isConclusionNow() ? this.queue[0].buyer : undefined;
+  }
+
+  getRemainTime() {
+    const time = new Date().getTime();
+    return this.operateTime - (time - this.startTime);
+  }
+
+  tryConclusion({ buyer, price }) {
+    this.queue.push({ buyer, price });
+  }
+
+  initQueue() {
+    this.queue = [];
+  }
+}
+
+class Auction {
+  constructor({ price, perPrice }) {
+    this.price = price;
+    this.perPrice = perPrice;
+  }
+
+  add() {
+    this.price += this.perPrice;
+  }
+
+  compare(currentPrice) {
+    return currentPrice === this.price;
+  }
+}
+
+module.exports = { Auction, Manage, AuctionHouse };
