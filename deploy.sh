b41169039d3a7e46fe619a02a5cf83eb5f761a08
GY KIM <59333136+crakel@users.noreply.github.com>
2022-12-08 20:47:25 +0900

    fix : deploy node version 2

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 echo "> pm2 배포"    >> /home/ubuntu/socket-deploy/deploy.log
-nvm use 16.18.0
+nvm use 16.18.0 >> /home/ubuntu/socket-deploy/deploy.log 2>/home/ubuntu/socket-deploy/deploy_err.log &
+npm i pm2 -g >> /home/ubuntu/socket-deploy/deploy.log 2>/home/ubuntu/socket-deploy/deploy_err.log &
 pm2 start /home/ubuntu/socket-deploy/bin/www >> /home/ubuntu/socket-deploy/deploy.log 2>/home/ubuntu/socket-deploy/deploy_err.log &
