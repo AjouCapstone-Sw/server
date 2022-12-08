cf36a028b567605851b1b2b29c05c862162d3091
GY KIM <59333136+crakel@users.noreply.github.com>
2022-12-08 20:51:23 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 echo "> pm2 배포"    >> /home/ubuntu/socket-deploy/deploy.log
+nvm install 16.18.0 >> /home/ubuntu/socket-deploy/deploy.log 2>/home/ubuntu/socket-deploy/deploy_err.log &
 nvm use 16.18.0 >> /home/ubuntu/socket-deploy/deploy.log 2>/home/ubuntu/socket-deploy/deploy_err.log &
 npm i pm2 -g >> /home/ubuntu/socket-deploy/deploy.log 2>/home/ubuntu/socket-deploy/deploy_err.log &
 pm2 start /home/ubuntu/socket-deploy/bin/www >> /home/ubuntu/socket-deploy/deploy.log 2>/home/ubuntu/socket-deploy/deploy_err.log &
