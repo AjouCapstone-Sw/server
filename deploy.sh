1b488fd99b976a681fb2f4f34d8254bd03f9f8ed
GY KIM <59333136+crakel@users.noreply.github.com>
2022-12-08 19:26:37 +0900

    fix : deploy log dir

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-echo "> pm2 배포"    >> /home/ubuntu/api-server/deploy.log
-pm2 start /home/ubuntu/socket-deploy/bin/www >> /home/ubuntu/deploy.log 2>/home/ubuntu/socket-deploy/deploy_err.log &
+echo "> pm2 배포"    >> /home/ubuntu/socket-deploy/deploy.log
+pm2 start /home/ubuntu/socket-deploy/bin/www >> /home/ubuntu/socket-deploy/deploy.log 2>/home/ubuntu/socket-deploy/deploy_err.log &
