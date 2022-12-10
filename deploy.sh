a140d9dcc7b290bfd8960add82a95af6e1dd816d
GY KIM <59333136+crakel@users.noreply.github.com>
2022-12-10 20:05:23 +0900

    fix : deploy nvm error

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 echo "> pm2 배포"    >> /home/ubuntu/socket-deploy/deploy.log
-nvm install 16.18.0 >> /home/ubuntu/socket-deploy/deploy.log 2>/home/ubuntu/socket-deploy/deploy_err.log &
-nvm use 16.18.0 >> /home/ubuntu/socket-deploy/deploy.log 2>/home/ubuntu/socket-deploy/deploy_err.log &
-npm i pm2 -g >> /home/ubuntu/socket-deploy/deploy.log 2>/home/ubuntu/socket-deploy/deploy_err.log &
-pm2 start /home/ubuntu/socket-deploy/bin/www >> /home/ubuntu/socket-deploy/deploy.log 2>/home/ubuntu/socket-deploy/deploy_err.log &
+
+export NVM_DIR="/home/ubuntu/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+# This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. 
+"$NVM_DIR/bash_completion"  
+# This loads nvm bash_completion
+
+nvm use 16
+
+cd /home/ubuntu/socket-deploy
+pm2 start bin/www >> /home/ubuntu/socket-deploy/deploy.log 2>/home/ubuntu/socket-deploy/deploy_err.log &
